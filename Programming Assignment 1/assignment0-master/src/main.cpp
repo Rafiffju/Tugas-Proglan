eef23069f15437434f36752c96756ef2ff0604a9
Rafif zzu <muhrafifizzuddin@gmail.com>
2026-07-03 19:11:45 +0700

    update tugas

---
+++ Programming Assignment 1/assignment0-master/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nama;
+    cout << "Silahkan inputkan nama Anda: " << endl;
+    cin >> nama;
+    cout << "Hello " << nama << endl;
+
+    return 0;
+}
---
+++ Programming Assignment 3/assignment2-main/assignment2-main/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "include/orang.hpp"
+#include "include/dosen.hpp"
+#include "include/mahasiswa.hpp"
+#include "include/tendik.hpp"
+
+using namespace std;
+using namespace sim;
+
+int main(int argc, char ** argv)
+{
+    
+
+    return 0;
+}
